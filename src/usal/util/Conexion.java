d70f2ce521426ab5cc918bdc24c0b6ad3627d6e3
Agustin <agustin.denicolas@usal.edu.ar>
2021-09-13 19:58:52 -0300

    Creacion del .properties y Conexion a la base de datos MySql

--- src/usal/util/Conexion.java
+++ src/usal/util/Conexion.java
@@ -1,35 +1,43 @@
 package usal.util;
 
-public class Conexion {	//TODO: Implemetar clase Conexion
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
-//	public static Conexion conectar() {
-//		Properties prop = new Properties();
-//		Conexion conn = null;
-//		try {
-//			prop.load(new FileReader("C:\\Users\\agden\\Desktop\\TEST-2Parcial\\DAO-Prueba\\Conf.properties"));
-////			Class.forName(prop.getProperty("jdbc_diver"));
-//			conn = DriverManager.getConnection(prop.getProperty("bd_url"));
-//			
-//		}catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		
-//		return conn;
-//	}
-//	
-//	public static void desconectar(Conexion conn) {
-//		if (conn != null) {
-//            try {
-//               conn.close();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//	}
-//	
-//	public static void main(String[] args) {
-//		PerroDAO dao = new PerroDAOImplMySQL();
-//		
-//		
-//	}
+public class Conexion {
+
+	public static Connection conectar() {
+		Properties prop = new Properties();
+		Connection conn = null;
+		try {
+			//Leemos el .properties
+			prop.load(new FileReader("C:\\Users\\agden\\Desktop\\AGUS\\FACULTAD\\"
+					+ "4 año\\Seminario de Integracion Profesional\\TP\\2do Cuatrimestre\\"
+					+ "CODIGO\\easyLawyer-DAO\\Conf.properties"));
+			
+			//Buscamos las creedenciales para conectar a la base de datos
+			String URL = prop.getProperty("bd_url");
+			String USER = prop.getProperty("bd_user");
+			String PASSWORD = prop.getProperty("bd_password");
+			
+//			Class.forName(prop.getProperty("jdbc_diver"));
+			conn = DriverManager.getConnection(URL, USER, PASSWORD);
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	public static void desconectar(Connection conn) {
+		if (conn != null) {
+            try {
+               conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+	}
 }
